547a9171a33ce68bab2c36ce8941d5d379c7d7e6
Christopher Murray <cwmurray@planview.com>
2021-04-22 12:00:48 -0500

    pulls envId and tenantId from document

--- OneDrive - Planview, Inc/Documents/GitHub/readTenantId/readTenantId.js
+++ OneDrive - Planview, Inc/Documents/GitHub/readTenantId/readTenantId.js
@@ -1,44 +1,18 @@
 const fs = require ('fs');
 const ini = require('ini');
+// const jenkins = require('jenkins')
 
-function parseINIString(data){
-    console.log(data)
-    var regex = {
-        section: /^\s*\[\s*([^\]]*)\s*\]\s*$/,
-        param: /^\s*([^=]+?)\s*=\s*(.*?)\s*$/,
-        comment: /^\s*;.*$/
-    };
-    var value = {};
-    var lines = data.split(/[\r\n]+/);
-    var section = null;
-    lines.forEach(function(line){
-        if(regex.comment.test(line)){
-            return;
-        }else if(regex.param.test(line)){
-            var match = line.match(regex.param);
-            if(section){
-                value[section][match[1]] = match[2];
-            }else{
-                value[match[1]] = match[2];
-            }
-        }else if(regex.section.test(line)){
-            var match = line.match(regex.section);
-            value[match[1]] = {};
-            section = match[1];
-        }else if(line.length == 0 && section){
-            section = null;
-        };
-    });
-    return value;
-}
-
-try {
-    console.log("initiated")
-let data = fs.readFileSync(`C:\\Users\\ChristopherMurray\\Documents\\PRM_Adapter_Config`)
-let ini = ini.parse(fs.readFileSync(`C:\\Users\\ChristopherMurray\\Documents\\PRM_Adapter_Config`))
-// let javascript_ini = parseINIString(data)
-console.log(ini)
+const readTenantId = (server, db, internalId)=>{
+try{
+    // `\\${server}\\F:\\Planview\\WebApp\\Objects\\PRM_Adapter_Config.ini`, "utf-8"
+let data =  ini.parse(fs.readFileSync(`C:\\Users\\ChristopherMurray\\Documents\\PRM_Adapter_Config.ini`, "utf-8"))
+let envId = data[`${db}`].hub_environment_id;
+let tenantId = data[`${db}`].hub_system_tenant_id;
+console.log("environment id:", envId)
+console.log("tenant id: ", tenantId)
 }
 catch(e) {
-    console.log(e);
+    console.log(e)
 }
+}
+readTenantId(null, 'REGSANDBOX1', null)
